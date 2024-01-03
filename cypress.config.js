8d5975bde1840f7d5f838a92975c031de01ac224
TulioIsaak <isaaktulio@gmail.com>
2024-01-03 16:49:23 -0300

    login and register tests

---
+++ cypress.config.js
@@ -0,0 +1,24 @@
+// import { defineConfig } from "cypress";
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: '8n7fn5',
+  e2e: {
+    setupNodeEvents(on, config) {
+      module.exports = defineConfig({
+        e2e: {
+            setupNodeEvents(on, config) {
+                { "reporter"; "mochawesome",
+                    "reporterOptions"; 
+                        { "reportDir"; "cypress/report/mochawesome-report",
+                        "overwrite"; true,
+                        "html"; true,
+                        "json"; false,
+                        "timestamp"; "mmddyyyy_HHMMss" }}
+            },
+        },
+    });
+    
+    },
+  },
+});
--- cypress/e2e/testando.cy.js
+++ cypress/e2e/testando.cy.js
@@ -1,11 +1,5 @@
-describe('Página de cadastro', () => {
-  it('Preencher os campos do formulário para cadastrar um novo usuário', () => {
-    cy.visit('http://localhost:4200/#/home')
-    cy.contains('a', 'Register now').click()
-    cy.get('[data-test="email"]').type('tisaak@gmail.com')
-    cy.get('[data-test="fullName"]').type('Túlio Isaak da Silva Barbosa')
-    cy.get('[data-test="registerUserName"]').type('tisaak')
-    cy.get('[data-test="registerPassword"]').type('12345678')
-    cy.contains('button', 'Register').click()
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
   })
 })
